5c6ff37e76ff77a168d5c300c80f7a861b10b7f3
mcpengelly <pengelly.mat@gmail.com>
2022-12-26 17:34:49 -0500

    more

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -4,8 +4,6 @@
 // we expect to get when we call `is_even(5)`.
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
@@ -16,11 +14,13 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        let some_num = 4;
+        assert!(is_even(some_num));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        let some_num = 5;
+        assert!(!is_even(some_num));
     }
 }
